e30fe26d7de7356bd42abf11f9aab9199159c4bf
Nadar-Suresh-Kumar <sureshkumarnadar1701@gmail.com>
2024-07-18 16:00:28 +0530

    he

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langchain import LLMChain, OpenAI
 from langchain.prompts import PromptTemplate
 
 # Set the REPLICATE_API_TOKEN environment variable
-os.environ["REPLICATE_API_TOKEN"] = "r8_6hI0lOyaOFtU8rfQaoSIsUcfaPnzcYV2D81gw"
+REPLICATE_API_TOKEN= "r8_6hI0lOyaOFtU8rfQaoSIsUcfaPnzcYV2D81gw"
 
 def generate_image(prompt, width, height, num_inference_steps):
     input = {
